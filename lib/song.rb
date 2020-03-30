29ab38ed5c9725ad08159b8eff0dfeb56a6d3d9b
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-30 00:14:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,12 @@ class Song
     self.all.uniq.sort_by {|inst| inst.name}
   end
 
-
+  def self.new_from_filename(filename)
+    artist_and_song = filename.split("-")
+    artist = artist_and_song[0]
+    song = self.create_by_name(artist_and_song[0...artist_and_song.length-4])
+    song.artist = artist
+    song
+  end
 
 end

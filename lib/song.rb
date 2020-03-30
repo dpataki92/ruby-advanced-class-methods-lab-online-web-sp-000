32f10ede76340a273c4b11896b605db21ef55b6d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-30 00:03:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,36 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = Song.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(str_name)
+    song = self.create
+    song.name = str_name
+    song
+  end
+
+  def self.create_by_name(str_name)
+    song = self.new_by_name(str_name)
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(str_name)
+    self.all.find {|inst| inst.name == str_name}
+  end
+
+  def self.find_or_create_by_name(str_name)
+    self.find_by_name(str_name) == nil ? self.create_by_name(str_name) : self.find_by_name(str_name)
+  end
+
+  def self.alphabetical
+    self.all.uniq.sort_by {|inst| inst.name}
+  end
+
+
+
 end
